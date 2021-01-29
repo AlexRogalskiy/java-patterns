27d85a6fcf224aea6930eda5e876a609923fb6c3
ADMSK\AVROGAL1 <avrogal1@mts.ru>
2021-01-29 23:40:45 +0300

    docs: updates on docs
    Added information on documentation

--- .github/scripts/comment-commands/close.sh
+++ .github/scripts/comment-commands/close.sh
@@ -3,7 +3,7 @@
 #doc: Close pending pull request temporary
 # shellcheck disable=SC2124
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-MESSAGE=$(cat $SCRIPT_DIR/../closing-message.txt)
+MESSAGE=$(cat "$SCRIPT_DIR"/../closing-message.txt)
 
 set +x #GITHUB_TOKEN
 curl -s -o /dev/null \
