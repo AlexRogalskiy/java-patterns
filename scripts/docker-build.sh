871f95659c4d597c8da814c73864f36615636238
Alexander Rogalskiy <arogalskii@sbereducation.ru>
2021-09-12 16:23:44 +0300

    Updates on files
    Added changeset on workflow

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
-set -e
+set -o errexit
+set -o nounset
+
+# Docker image params
+readonly IMAGE_REPOSITORY="styled-java-patterns"
+readonly IMAGE_TAG="latest"
+readonly GIT_SHA=$(git rev-parse HEAD)
 
 cd "$(dirname "$0")" || exit 1
 
-# Docker image params
-GIT_SHA=$(git rev-parse HEAD)
-IMAGE_TAG="styled-java-patterns"
+main() {
+  echo 'Building docker container...'
 
-# Build docker image
-docker build -f Dockerfile -t "${IMAGE_TAG}" -t "${IMAGE_TAG}:${GIT_SHA}" .
+  # Build docker image
+  docker build -f Dockerfile -t "${IMAGE_REPOSITORY}:${IMAGE_TAG}" -t "${IMAGE_REPOSITORY}:${GIT_SHA}" .
+}
+
+main
---
+++ scripts/docker-run.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+
+# Docker image params
+readonly IMAGE_REPOSITORY="styled-java-patterns"
+readonly IMAGE_TAG="latest"
+readonly REPO_ROOT="${REPO_ROOT:-$(git rev-parse --show-toplevel)}"
+
+cd "$(dirname "$0")" || exit 1
+
+main() {
+  echo 'Running docker container...'
+
+  # Run docker image
+  config_container_id=$(create_docker_container)
+
+  # shellcheck disable=SC2064
+  trap "cleanup ${config_container_id}" EXIT
+
+#  configure_kubectl "${config_container_id}"
+}
+
+cleanup() {
+  echo 'Removing docker container...'
+
+  config_container_id=$1
+  docker container rm --force $config_container_id > /dev/null
+  #docker kill ct > /dev/null 2>&1
+
+  echo 'Done!'
+}
+
+configure_kubectl() {
+  container_id="$1"
+
+  apiserver_id=$(lookup_apiserver_container_id)
+  if [[ -z "$apiserver_id" ]]; then
+      echo "ERROR: API-Server container not found. Make sure 'Show system containers' is enabled in Docker4Mac 'Preferences'!" >&2
+      exit 1
+  fi
+
+  ip=$(get_docker_args "$apiserver_id" --advertise-address)
+  port=$(get_docker_args "$apiserver_id" --secure-port)
+}
+
+lookup_apiserver_container_id() {
+    docker container list --filter name=k8s_kube-apiserver --format '{{ .ID }}'
+}
+
+get_docker_args() {
+  container_id="$1"
+  arg="$2"
+
+  docker container inspect "$container_id" | jq -r ".[].Args[] | capture(\"$arg=(?<arg>.*)\") | .arg"
+}
+
+create_docker_container() {
+  docker run -ti --rm \
+            -v "$REPO_ROOT:/usr/src/app" \
+            -e CI=1 \
+            -e CI=1 \
+            "${IMAGE_REPOSITORY}:${IMAGE_TAG}" build --strict
+}
+
+main
