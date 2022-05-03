84c96d1543d20e89008903c2d6cebcbb9c2a61d7
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2022-05-03 18:27:04 +0300

    Updates on docs

--- commitlint.config.js
+++ commitlint.config.js
@@ -15,4 +15,11 @@
  */
 'use strict';
 
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+	extends: ['@commitlint/config-conventional'],
+	rules: {
+		'body-max-line-length': [2, 'always', 120],
+		'footer-max-line-length': [2, 'always', 120],
+		'header-max-length': [2, 'always', 72],
+	},
+};
