86ab0c045b24c572a57d2122681c2a4a79d6388d
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2022-09-29 11:56:06 +0300

    Updates on docs

--- changelog.config.js
+++ changelog.config.js
@@ -15,7 +15,7 @@
  */
 'use strict';
 
-export default {
+module.exports = {
 	disableEmoji: false,
 	list: ['test', 'feat', 'fix', 'chore', 'docs', 'refactor', 'style', 'ci', 'perf'],
 	maxMessageLength: 64,
